ff2759acda55602c77dd806a1c92d834be8cdfaf
akshitkrnagpal <akshitkrnagpal@gmail.com>
2017-05-07 02:33:46 +0530

    Moved profile info to profile.json

--- src/index.js
+++ src/index.js
@@ -4,43 +4,7 @@ import './index.css';
 
 import ProfileCard from './components/profile-card.js';
 
-var profile = {
-	profileImage : 'images/profile-image.png',
-	name : {
-		firstname : 'Akshit',
-		lastname  : 'Kr Nagpal'
-	},
-	titles : [
-		'Programmer',
-		'Coder',
-		'Developer'
-	],
-	socialLinks : [
-		{
-			link : 'https://www.facebook.com/akshitkrnagpal' ,
-			icon : 'images/social-icons/facebook.png',
-			alt  : 'facebook'
-		},
-		{
-			link : 'https://github.com/AkshitKrNagpal' ,
-			icon : 'images/social-icons/github.png',
-			alt  : 'github'
-		},
-		{
-			link : 'https://www.linkedin.com/in/akshit-kr-nagpal-40187110a' ,
-			icon : 'images/social-icons/linkedin.png',
-			alt  : 'linkedin'
-		}
-	],
-	scholastic : {
-		title : 'Research Scholar',
-		place : 'Cluster Innovation Centre',
-		link  : 'https://ducic.ac.in/'
-	},
-	email : 'akshitkrnagpal@gmail.com',
-	resumeLink : 'resume.pdf'
-
-}
+var profile = require('./profile.json');
 
 ReactDOM.render(
   <ProfileCard profile={profile} />,
