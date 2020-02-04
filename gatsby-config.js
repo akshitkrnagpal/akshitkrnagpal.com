a2579c43e8079f47e31764eb5589bb68f00206b7
akshitkrnagpal <akshitkrnagpal@gmail.com>
2020-02-04 12:47:56 +0530

    Remove PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,17 +53,6 @@ module.exports = {
             },
         },
         `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-plugin-offline`,
-            options: {
-                runtimeCaching: [
-                    {
-                        // Google Fonts CSS (doesn't end in .css so we need to specify it)
-                        urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-                        handler: `staleWhileRevalidate`,
-                    },
-                ],
-            },
-        },
+        `gatsby-plugin-remove-serviceworker`,
     ],
 };
