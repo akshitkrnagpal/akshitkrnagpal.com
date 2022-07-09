df672f0739dbc42974f58f327865e26dabe6e496
akshitkrnagpal <akshitkrnagpal@gmail.com>
2022-07-09 23:28:55 +0530

    Init turborepo

--- README.md
+++
@@ -1,3 +0,0 @@
-# AkshitKrNagpal.com
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/f9e2d91d-cb09-4baa-8ea5-bcbc0544af8c/deploy-status)](https://app.netlify.com/sites/akshitkrnagpal/deploys)
---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Akshit Kr Nagpal</title>
+        <meta name="description" content="Independent Creative Developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hi. I am Akshit.</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
