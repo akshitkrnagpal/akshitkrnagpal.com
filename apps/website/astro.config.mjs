c7d06b700a405b41d775ee68013e2590dc99797d
akshitkrnagpal <akshitkrnagpal@gmail.com>
2022-09-10 18:07:41 +0530

    Add compression plugin

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [
+    react(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    compress(),
+  ],
 });
