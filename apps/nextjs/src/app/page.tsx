9105b5f80aa004bb7bb0f5ed83d95947685a2116
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-10-15 06:32:42 +0530

    Update animation for text

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -14,9 +14,11 @@ export default function Home() {
         <motion.div
           initial={{
             opacity: 0,
+            y: 50,
           }}
           animate={{
             opacity: 1,
+            y: 0,
           }}
           transition={{
             duration: 0.5,
