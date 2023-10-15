e1456d11aa98a31dbaef60c425db974aae8d2d67
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-10-15 18:52:22 +0530

    Decreased bundle size

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -3,9 +3,15 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
-import { SocialIcon } from "react-social-icons";
 import image from "@/images/image.jpg";
 import { title } from "@akshitkrnagpal/data";
+
+// @ts-expect-error - The types for this package are not present for /component
+import { SocialIcon } from "react-social-icons/component";
+
+import "react-social-icons/linkedin";
+import "react-social-icons/github";
+import "react-social-icons/twitter";
 
 export default function Home() {
   return (
